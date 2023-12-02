039c4467ee9eb3276c6630a1b33413b69960efda
iamtongfei <tongfei.g@columbia.edu>
2023-12-02 12:52:47 -0500

    implement detector

--- src/module1.py
+++ src/module1.py
@@ -1,7 +1,9 @@
 class Detector:
-    
     def __init__(self, path):
         self.path = path
-    
+
     def generate_results(self):
-        ...
+        # Implement your code here to generate results using the provided path
+        # For example:
+        print("start the work now 12:52")
+        # Your detection logic goes here
